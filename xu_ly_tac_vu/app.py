32b44081a159c836cce0f6289f245979ee62af09
vuivene <105068620+vuivene@users.noreply.github.com>
2026-06-06 02:59:42 +0700

    Update app.py

--- xu_ly_tac_vu/app.py
+++ xu_ly_tac_vu/app.py
@@ -176,4 +176,5 @@ def logout():
 
 if __name__ == '__main__':
     # Chạy trên môi trường Local Host Development
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
